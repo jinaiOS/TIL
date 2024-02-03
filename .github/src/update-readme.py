9639f3b4aa49655e1ac48019acb6b43c3ba62114
김징니 <105254025+jinaiOS@users.noreply.github.com>
2024-02-03 09:32:09 +0900

    Update update-readme.py

--- .github/src/update-readme.py
+++ .github/src/update-readme.py
@@ -5,11 +5,38 @@ import re
 # 탐색할 root 경로
 dir_path = "."
 
+# ignore 파일을 읽어서 패턴 목록을 리스트로 저장
+with open('./.github/config/.ignore', 'r') as f:
+    ignore = f.readlines()
+patterns = []
+for p in ignore:
+    pattern = r""
+    for c in p.strip():
+        if c == '*':
+            pattern = pattern + r".*"
+        elif c in ".^$*+?{}[]|()":  # 메타 문자
+            pattern = pattern + r"[{}]".format(c)
+        else:
+            pattern = pattern + r'{}'.format(c)
+    patterns.append(pattern)
+
+    
+# ignore 패턴과 일치하는지 확인하는 함수
+def check_ignore_pattern(item_path):
+    for pattern in patterns:
+        if re.fullmatch(pattern, item_path[(len(dir_path)+1):]):  # 항상 붙는 "[dir_path]/" 제거
+            return True
+    return False
+
+
 def find_target(path, level):
     file_list = []
     # 하위 디렉토리 순환
     for item in os.listdir(path):
         item_path = os.path.join(path, item)
+        
+        if check_ignore_pattern(item) or check_ignore_pattern(item_path): # 파일 이름이나 경로가 ignore 조건을 만족하면 무시
+            continue
             
         # 파일(혹은 디렉토리) 리스트에 추가하기
         mtime = datetime.fromtimestamp(os.stat(item_path).st_mtime)  # 수정 날짜 가져오기
@@ -76,3 +103,9 @@ with open("README.md", "w") as f:
         f.write("### [{}](#{})\n".format(file[0], file[0]))
         print_file_list(f, file[3], 0)
         f.write("\n")
+
+    '''for target in file_list:
+        for level in range(target[0]):
+            f.write("  ")
+        
+        f.write("- [{}](\"{}\") - {}\n".format(target[1], target[2].replace(' ', '_'), target[3]))'''
