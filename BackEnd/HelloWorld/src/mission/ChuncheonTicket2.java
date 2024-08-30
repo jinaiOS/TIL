26e8e8b0a75c81e54177aaa89d480622c15b5670
jinaiOS <kj227777@naver.com>
2024-08-30 11:30:33 +0900

    java 미션 2, 미션 3 클리어

--- BackEnd/HelloWorld/src/mission/ChuncheonTicket2.java
+++ BackEnd/HelloWorld/src/mission/ChuncheonTicket2.java
@@ -1,17 +1,17 @@
 package mission;
 
 public class ChuncheonTicket2 {
-    /**
-     * Q2) 이 티켓 값은 얼마일까?
-     * 전문가 4명이 제시하는 적정 티켓 값의 평균을 티켓 값으로 최종 책정하려고 한다.
-     * 각각 제시한 티켓 값 Kim: 3000, Park: 5000, Lee: 2000, Hone: 2000
-     */
     public static void main(String[] args) {
-        int freeTicket = 625;
-        int leftTicket = freeTicket % 3;
-        int recipients = freeTicket / 3;
+        /**
+         * Q2) 최대 몇 명까지 나눠줄 수 있고, 그 때 몇 장의 티켓이 남는가? (아주 인기가 많을 예정)
+         * 시립 박물관 오픈기념으로 시청 근무자들에게 박물관 티켓 1112장 을 무료로 배포하려고 한다.
+         * 이 때, 각각 개인은 ”4＂장의 티켓을 가져간다.
+         */
+        int freeTickets = 1111;
+        int recipients = freeTickets / 4;
+        int leftTicket = freeTickets % 4;
 
-        System.out.println(leftTicket);
-        System.out.println(recipients);
+        System.out.println("티켓을 가져 갈수 있는 근무자들 수는? " + recipients);
+        System.out.println("티켓을 다 나눠주고, 남는 티켓 수는? " + leftTicket);
     }
 }
--- BackEnd/HelloWorld/src/mission/ChuncheonTicket3.java
+++ BackEnd/HelloWorld/src/mission/ChuncheonTicket3.java
@@ -1,12 +1,31 @@
 package mission;
 
 public class ChuncheonTicket3 {
-    /**
-     * Q1) 이 티켓 값은 얼마일까?
-     * 전문가 4명이 제시하는 적정 티켓 값의 평균을 티켓 값으로 최종 책정하려고 한다.
-     * 각각 제시한 티켓 값 Kim: 3000, Park: 5000, Lee: 2000, Hone: 2000
-     */
     public static void main(String[] args) {
+        /**
+         * Q) 서로 다른 3명의 손님이 왔다. 3명은 반값 할인 적용 대상인가? 아닌가?
+         * 나이가 65세 이상이고, 국적이 ＂한국”이면 반값으로 줄 예정이다.
+         * Alice(나이: 70, 나라: 미국)
+         * Kim (나이: 65, 나라: 한국)
+         * Yoo (나이: 42, 나라: 한국)
+         */
+        int ageOfAlice = 70;
+        String countryOfAlice = "USA";
+        Boolean resultOfAlice = ageOfAlice >= 65 && countryOfAlice == "Korea";
 
+        int ageOfKim = 65;
+        String countryOfKim = "Korea";
+        Boolean resultOfKim = ageOfKim >= 65 && countryOfKim == "Korea";
+
+        int ageOfYoo = 42;
+        String countryOfYoo = "Korea";
+        Boolean resultOfYoo = ageOfYoo >= 65 && countryOfYoo == "Korea";
+
+        System.out.println("Alice는 적용 대상자인가?");
+        System.out.println(resultOfAlice);
+        System.out.println("Kim는 적용 대상자인가?");
+        System.out.println(resultOfKim);
+        System.out.println("Yoo는 적용 대상자인가?");
+        System.out.println(resultOfYoo);
     }
 }
