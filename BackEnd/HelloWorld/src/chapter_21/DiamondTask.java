1b0ad25576276d9daa0e7ad5d1eb3b4928417b76
jinaiOS <kj227777@naver.com>
2024-09-03 09:59:49 +0900

    java 메소드

---
+++ BackEnd/HelloWorld/src/chapter_21/DiamondTask.java
@@ -0,0 +1,27 @@
+package chapter_21;
+
+public class DiamondTask {
+    public static void main(String[] args) {
+        int size = 10;
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = i; j < size; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = size - 1; i >= 1; i--) {
+            for (int j = size; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
