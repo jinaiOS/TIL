c2e4f522f5ca0f7b3b5e3f8906de95436321d543
jinaiOS <kj227777@naver.com>
2024-09-25 09:42:32 +0900

    Java JVM 메모리 구조

---
+++ BackEnd/HelloWorld/src/chapter_40/Staff.java
@@ -0,0 +1,27 @@
+package chapter_40;
+
+public class Staff {
+    // 속성
+    private int salesAmount;
+
+    // 행위
+    public int helpPayment(Customer customer, int price) {
+        return customer.calculatePrice(price);
+    }
+
+    public int helpPayment(VIPCustomer vipCustomer, int price) {
+        return vipCustomer.calculatePrice(price);
+    }
+
+    public void addSalesAmount(int cash) {
+        salesAmount += cash;
+    }
+
+    public void printMySalesAmount() {
+        System.out.println("오늘의 최종 매상은 " + this.salesAmount);
+    }
+
+    public void setSalesAmount(int salesAmount) {
+        this.salesAmount = salesAmount;
+    }
+}
