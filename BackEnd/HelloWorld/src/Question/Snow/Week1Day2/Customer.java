3474f91fc2c15109da32c3f5cb29bf6278fb51c0
jinaiOS <kj227777@naver.com>
2024-09-29 10:52:59 +0900

    슈퍼코딩 스노우 1주차 2일

---
+++ BackEnd/HelloWorld/src/Question/Snow/Week1Day2/Customer.java
@@ -0,0 +1,9 @@
+package Question.Snow.Week1Day2;
+
+class Customer {
+    static int serialNum = 1;
+    private String name;
+    public Customer(String name) {
+        this.name = name;
+    }
+}
